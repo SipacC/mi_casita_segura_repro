769b6fbb4e869b1b965cef053fa920823e6ef8c9
jose <sipacchuquiejj@gmail.com>
2025-08-03 19:20:47 -0600

    Actualizar conexión para usar variables de entorno en Render

--- src/java/Config/Conexion.java
+++ src/java/Config/Conexion.java
@@ -7,15 +7,15 @@ public class Conexion {
     Connection con;
     public Conexion(){
         try {
+            String url = System.getenv("DB_URL");
+            String user = System.getenv("DB_USER");
+            String password = System.getenv("DB_PASSWORD");
+            
             Class.forName("org.postgresql.Driver");
-            con=DriverManager.getConnection(
-                    "jdbc:postgresql://dpg-d27sspu3jp1c73fnei50-a.oregon-postgres.render.com:5432/mi_casita_segura",
-                    "admin",
-                    "u410rSBv0A4J7cosYMBmybWSN1ouZUv4"
-            );
-            System.out.println("Conexión a PostgreSQL exitosa");
+            con = DriverManager.getConnection(url, user, password);
+            System.out.println("Conexión PostgreSQL exitosa");
         } catch (Exception e) {
-            System.err.println("Error"+e);
+            System.err.println("Error en conexión PostgreSQL: " + e.getMessage());
         }
     }
     public Connection getConnection(){
